c657986f8d5ed48d98583dbc2bee75fea84090a1
Juan Rambal <juanp231100@gmail.com>
2020-10-22 23:16:45 -0500

    feat: swagger module and new packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
+import { DatabaseModule } from './config/database/database.module';
+import { TweetsModule } from './routes/tweets/tweets.module';
+import { UsersModule } from './routes/users/users.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    TweetsModule,
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
+import {
+  NestExpressApplication,
+  ExpressAdapter,
+} from '@nestjs/platform-express';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+} from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as auth from 'express-basic-auth';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(),
+  );
+  app.enableCors({ origin: '*' });
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(
+    '/doc',
+    auth({
+      challenge: true,
+      users: { pf: `${process.env.SWAGGER_PASS}` },
+    }),
+  );
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Proyecto Final API')
+    .setDescription('Documentación de la api de proyecto final 2020')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none',
+    },
+    customCssUrl:
+      'https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-monokai.css',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
